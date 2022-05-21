ec6bca2d93e3bdb451bf4181cd2f77c86c6ee0f1
graciagarpin <graciagarpin@gmail.com>
2022-05-21 11:48:31 +0200

    add filter by gender and run publish react

--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -1,9 +1,14 @@
 // login
 
-const getMoviesFromApi = () => {
+const getMoviesFromApi = (props) => {
   console.log('Se están pidiendo las películas de la app');
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('//localhost:4000/movies') // aqui ponemos la direccion del servidor que vamos a crear
+  return fetch(`//localhost:4000/movies?gender=${props.gender}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
     .then((response) => response.json())
     .then((data) => {
       return data;
